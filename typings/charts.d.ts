5a0700c383fd57961af3283b9a0be2ac5862be2d
Edwin P. Magezi <emagezi@gmail.com>
2017-08-17 17:05:33 +0300

    Move plotly.js types to typings folder

--- typings/charts.d.ts
+++ typings/charts.d.ts
@@ -1,6 +1,5 @@
 declare function require(name: string): string;
-declare module "plotly.js/dist/plotly" {
-    const plotly: Plotly;
 
-    export = plotly;
+declare module "plotly.js/dist/plotly" {
+    export = Plotly;
 }
