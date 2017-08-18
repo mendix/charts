5175afd631e60c673928e2fd65534feea4249217
Edwin P. Magezi <emagezi@gmail.com>
2017-08-18 12:31:21 +0300

    Add default line chart

---
+++ src/LineChart/components/LineChart.ts
@@ -0,0 +1,96 @@
+import { Component, createElement } from "react";
+import * as classNames from "classnames";
+
+import * as Plotly from "plotly.js/dist/plotly";
+import { Dimensions, getDimensions } from "../../utils/style";
+
+interface LineChartProps extends Dimensions {
+    data?: Plotly.ScatterData[];
+    config?: Partial<Plotly.Config>;
+    layout?: Partial<Plotly.Layout>;
+    className?: string;
+    style?: object;
+}
+
+export class LineChart extends Component<LineChartProps, {}> {
+    private lineChart: HTMLDivElement;
+    private data = [
+        {
+            connectgaps: true,
+            mode: "lines+markers",
+            name: "Default data",
+            type: "scatter",
+            x: [ 14, 20, 30, 50 ],
+            y: [ 14, 30, 20, 40 ]
+        }
+    ] as Plotly.ScatterData[];
+
+    constructor(props: LineChartProps) {
+        super(props);
+
+        this.getPlotlyNodeRef = this.getPlotlyNodeRef.bind(this);
+        this.onResize = this.onResize.bind(this);
+    }
+
+    render() {
+        return createElement("div", {
+            className: classNames("widget-charts-line", this.props.className),
+            ref: this.getPlotlyNodeRef,
+            style: { ...getDimensions(this.props), ...this.props.style }
+        });
+    }
+
+    componentDidMount() {
+        this.renderChart(this.props);
+        this.setUpResizeEvents();
+        this.adjustStyle();
+    }
+
+    componentWillReceiveProps(newProps: LineChartProps) {
+        this.renderChart(newProps);
+    }
+
+    componentWillUnmount() {
+        if (this.lineChart) {
+            Plotly.purge(this.lineChart);
+        }
+        window.removeEventListener("resize", this.onResize);
+    }
+
+    private getPlotlyNodeRef(node: HTMLDivElement) {
+        this.lineChart = node;
+    }
+
+    private adjustStyle() {
+        if (this.lineChart) {
+            const wrapperElement = this.lineChart.parentElement;
+            if (this.props.heightUnit === "percentageOfParent" && wrapperElement) {
+                wrapperElement.style.height = "100%";
+                wrapperElement.style.width = "100%";
+            }
+        }
+    }
+
+    private setUpResizeEvents() {
+        // A workaround for attaching the resize event to the Iframe window because the plotly
+        // library does not support it. This fix will be done in the web modeler preview class when the
+        // plotly library starts supporting listening to Iframe events.
+        const iFrame = document.getElementsByClassName("t-page-editor-iframe")[0] as HTMLIFrameElement;
+        if (iFrame) {
+            (iFrame.contentWindow || iFrame.contentDocument).addEventListener("resize", this.onResize);
+        } else {
+            window.addEventListener("resize", this.onResize);
+        }
+    }
+
+    private renderChart(props: LineChartProps) {
+        const data = props.data && props.data.length ? props.data : this.data;
+        if (this.lineChart) {
+            Plotly.newPlot(this.lineChart, data, this.props.layout, this.props.config);
+        }
+    }
+
+    private onResize() {
+        Plotly.Plots.resize(this.lineChart);
+    }
+}
--- src/LineChart/components/LineChartContainer.ts
+++ src/LineChart/components/LineChartContainer.ts
@@ -1,10 +1,111 @@
-// tslint:disable no-console
 import { Component, createElement } from "react";
+import { LineChart } from "./LineChart";
+import { Dimensions, parseStyle } from "../../utils/style";
 
-class LineChartContainer extends Component<{}, {}> {
-    render() {
-        return createElement("div", {}, "Line Chart");
-    }
+interface WrapperProps {
+    "class"?: string;
+    mxform: mxui.lib.form._FormBase;
+    mxObject?: mendix.lib.MxObject;
+    style?: string;
+    readOnly: boolean;
 }
 
-export { LineChartContainer as default };
+export interface LineChartContainerProps extends WrapperProps, Dimensions {
+    seriesEntity: string;
+    seriesNameAttribute: string;
+    dataEntity: string;
+    dataSourceType: "xpath" | "microflow";
+    entityConstraint: string;
+    dataSourceMicroflow: string;
+    xValueAttribute: string;
+    yValueAttribute: string;
+    xAxisSortAttribute: string;
+    mode: "lines" | "markers" | "text" | "linesomarkers";
+    lineColor: string;
+    showGrid: boolean;
+    showToolBar: boolean;
+    showLegend: boolean;
+    responsive: boolean;
+    xAxisLabel: string;
+    yAxisLabel: string;
+}
+
+interface LineChartContainerState {
+    alertMessage?: string;
+    data?: Plotly.ScatterData[];
+}
+
+export default class LineChartContainer extends Component<LineChartContainerProps, LineChartContainerState> {
+    private subscriptionHandle: number;
+
+    constructor(props: LineChartContainerProps) {
+        super(props);
+
+        this.state = {
+            alertMessage: LineChartContainer.validateProps(this.props),
+            data: []
+        };
+        this.fetchData = this.fetchData.bind(this);
+    }
+
+    render() {
+        return createElement(LineChart, {
+            className: this.props.class,
+            config: {
+                displayModeBar: this.props.showToolBar
+            },
+            data: this.state.data,
+            layout: {
+                autosize: this.props.responsive,
+                showlegend: this.props.showLegend,
+                xaxis: {
+                    showgrid: this.props.showGrid,
+                    title: this.props.xAxisLabel
+                },
+                yaxis: {
+                    showgrid: this.props.showGrid,
+                    title: this.props.yAxisLabel
+                }
+            },
+            style: parseStyle(this.props.style),
+            width: this.props.width,
+            height: this.props.height,
+            widthUnit: this.props.widthUnit,
+            heightUnit: this.props.heightUnit
+        });
+    }
+
+    componentWillReceiveProps(newProps: LineChartContainerProps) {
+        this.resetSubscriptions(newProps.mxObject);
+        this.fetchData(newProps.mxObject);
+    }
+
+    componentWillUnmount() {
+        if (this.subscriptionHandle) {
+            mx.data.unsubscribe(this.subscriptionHandle);
+        }
+    }
+
+    public static validateProps(props: LineChartContainerProps): string {
+        return props.dataSourceType === "microflow" && !props.dataSourceMicroflow
+            ? "Configuration error: data source type is set to 'Microflow' but 'Source microflow' is missing"
+            : "";
+    }
+
+    private resetSubscriptions(mxObject?: mendix.lib.MxObject) {
+        if (this.subscriptionHandle) {
+            mx.data.unsubscribe(this.subscriptionHandle);
+        }
+
+        if (mxObject) {
+            this.subscriptionHandle = mx.data.subscribe({
+                callback: () => this.fetchData(mxObject),
+                guid: mxObject.getGuid()
+            });
+        }
+    }
+
+    private fetchData(mxObject?: mendix.lib.MxObject) {
+        console.log(mxObject); // tslint:disable-line
+    }
+}
