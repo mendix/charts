04d5c365f8838473b5b728881b91c9eaebf4eb9a
Edwin P. Magezi <emagezi@gmail.com>
2017-11-22 11:39:35 +0300

    Add PlotlyChart unit tests

---
+++ src/__tests__/PlotlyChart.spec.ts
@@ -0,0 +1,13 @@
+describe("PlotlyChart", () => {
+    xit ("renders the structure correctly", () => {
+        //
+    });
+
+    xit ("renders the chart", () => {
+        //
+    });
+
+    xit ("resizes the chart when the parent is resized", () => {
+        //
+    });
+});
