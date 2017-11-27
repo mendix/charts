f669ac8ca31e4737d6fcb26a56417ff5bd53b5fa
Edwin P. Magezi <emagezi@gmail.com>
2017-11-27 12:12:25 +0300

    Add separate webpack configs for each chart

--- webpack.config.js
+++ webpack.config.js
@@ -5,67 +5,64 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 const widgetName = require("./package").widgetName;
 
-const widgetConfig = {
-    entry: {
-        ColumnChart: "./src/ColumnChart/components/ColumnChartContainer.ts",
-        LineChart: "./src/LineChart/components/LineChartContainer.ts",
-        PieChart: "./src/PieChart/components/PieChartContainer.ts"
-    },
-    output: {
-        path: path.resolve(__dirname, "dist/tmp"),
-        filename: "src/com/mendix/widget/custom/[name]/[name].js",
-        chunkFilename: `src/com/mendix/widget/custom/${widgetName}[id].js`,
-        libraryTarget: "umd"
-    },
-    resolve: {
-        extensions: [ ".ts", ".js" ],
-        alias: {
-            "tests": path.resolve(__dirname, "./tests")
-        }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: "ts-loader"
-            },
-            {
-                test: /\.css$/, loader: ExtractTextPlugin.extract({
+const chartNames = [ "ColumnChart", "LineChart", "PieChart" ];
+const createConfig = (chartName) => {
+    return {
+        entry: `./src/${chartName}/components/${chartName}Container.ts`,
+        output: {
+            path: path.resolve(__dirname, "dist/tmp"),
+            filename: `src/com/mendix/widget/custom/${chartName.toLowerCase()}/${chartName}.js`,
+            chunkFilename: `src/com/mendix/widget/custom/${widgetName}[id].js`,
+            libraryTarget: "umd"
+        },
+        resolve: {
+            extensions: [ ".ts", ".js" ],
+            alias: {
+                "tests": path.resolve(__dirname, "./tests")
+            }
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    use: "ts-loader"
+                },
+                {
+                    test: /\.css$/, loader: ExtractTextPlugin.extract({
                     fallback: "style-loader",
                     use: "css-loader"
                 })
-            },
-            {
-                test: /\.scss$/,
-                loader: ExtractTextPlugin.extract({
-                    fallback: "style-loader",
-                    use: "css-loader!sass-loader"
-                })
-            }
+                },
+                {
+                    test: /\.scss$/,
+                    loader: ExtractTextPlugin.extract({
+                        fallback: "style-loader",
+                        use: "css-loader!sass-loader"
+                    })
+                }
+            ]
+        },
+        externals: [ "react", "react-dom", {
+            [/PlotlyCustom/]: "widgets/com/mendix/widget/custom/charts/PlotlyCustom.js"
+        } ],
+        plugins: [
+            new CopyWebpackPlugin([
+                { from: "src/**/*.js" },
+                { from: "src/*.xml" },
+                { from: `src/${chartName}/*.xml` },
+                { from: "src/**/*.png", to: `src/com/mendix/widget/custom/${chartName.toLowerCase()}/` }
+            ], {
+                copyUnmodified: true
+            }),
+            new ExtractTextPlugin({ filename: `./src/com/mendix/widget/custom/${chartName.toLowerCase()}/ui/[name].css` }),
+            new webpack.LoaderOptionsPlugin({
+                debug: true
+            }),
+            new webpack.SourceMapDevToolPlugin({
+                filename: `./src/com/mendix/widget/custom/${chartName.toLowerCase()}/${chartName}.js.map`
+            })
         ]
-    },
-    externals: [ "react", "react-dom", {
-        [/PlotlyCustom/]: "widgets/com/mendix/widget/custom/charts/PlotlyCustom.js"
-    } ],
-    plugins: [
-        new CopyWebpackPlugin([
-            { from: "src/**/*.js" },
-            { from: "src/*.xml" },
-            { from: "src/LineChart/*.xml" },
-            { from: "src/ColumnChart/*.xml" },
-            { from: "src/PieChart/*.xml" },
-            { from: "src/**/*.png", to: `src/com/mendix/widget/custom/[name]/` }
-        ], {
-            copyUnmodified: true
-        }),
-        new ExtractTextPlugin({ filename: `./src/com/mendix/widget/custom/[name]/ui/[name].css` }),
-        new webpack.LoaderOptionsPlugin({
-            debug: true
-        }),
-        new webpack.SourceMapDevToolPlugin({
-            filename: './src/com/mendix/widget/custom/[name]/[name].js.map'
-        })
-    ]
+    }
 };
 
 const plotlyCustomConfig = {
@@ -114,4 +111,4 @@ const previewConfig = {
     }) ]
 };
 
-module.exports = [ widgetConfig, plotlyCustomConfig, previewConfig ];
+module.exports = chartNames.map(chartName => createConfig(chartName)).concat([ plotlyCustomConfig, previewConfig ]);
