e18404ccc03444172639ed5bc46bf662926a2883
edwinmp <edwinm_p@yahoo.com>
2018-04-18 10:57:32 +0300

    Add convert JS to JSON to AnyChart

--- src/AnyChart/components/AnyPlayground.ts
+++ src/AnyChart/components/AnyPlayground.ts
@@ -151,8 +151,10 @@ export class AnyPlayground extends Component<AnyChartProps, AnyPlaygroundState> 
         }
         this.timeoutId = window.setTimeout(() => {
             try {
-                if (this.isValid && JSON.parse(value)) {
+                if (this.isValid) {
                     this.updateChart(source, JSON.stringify(JSON.parse(value), null, 2));
+                } else {
+                    this.updateChart(source, Playground.convertJSToJSON(value));
                 }
             } catch (error) {
                 this.isValid = false;
