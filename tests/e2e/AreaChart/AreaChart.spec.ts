0eb6d86516a3a3eef3d2366ad94bb5858f5d9995
Andries Smit <mr.andries.smit@gmail.com>
2018-10-08 13:24:13 +0200

    Shorten wait e2e

--- tests/e2e/AreaChart/AreaChart.spec.ts
+++ tests/e2e/AreaChart/AreaChart.spec.ts
@@ -3,11 +3,11 @@ import chart from "./pages/area.page";
 describe("Area chart", () => {
     beforeAll(() => {
         chart.open();
+        browser.pause(3000);
     });
 
     it("should generate a chart", () => {
         chart.svgElement.waitForVisible();
-        browser.pause(10000);
         const isExist = chart.svgElement.isExisting();
 
         expect(isExist).toBeTruthy();
@@ -15,7 +15,6 @@ describe("Area chart", () => {
 
     it("should be generated with two traces", () => {
         chart.traces.waitForValue();
-        browser.pause(10000);
 
         expect(chart.traces.value.length).toBe(2);
     });
