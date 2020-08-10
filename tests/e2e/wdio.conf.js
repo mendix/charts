4e9367561b31cd2f4fa67c06e32481404c9bab09
I. Isaku <16718722+IIsaku@users.noreply.github.com>
2020-08-10 14:35:32 +0200

    Fix travis

--- tests/e2e/wdio.conf.js
+++ tests/e2e/wdio.conf.js
@@ -1,48 +1,51 @@
 const debug = process.env.DEBUG;
-const multiclient = process.env.MULTCLIENT;
+const browser = process.env.BROWSER || "firefox";
 
 exports.config = {
     host: "127.0.0.1",
     port: 4444,
-    specs: [ "./dist/e2e/**/*.spec.js" ],
+    specs: ["./dist/e2e/**/*.spec.js"],
     maxInstances: 1,
-    capabilities: multiclient
-        ? [
-            { browserName: "chrome" },
-            { browserName: "firefox", marionette: true },
-            { browserName: "internet explorer", ignoreProtectedModeSettings: true, ignoreZoomSetting: true },
-            { browserName: "MicrosoftEdge", elementScrollBehavior: 1, nativeEvents: false }
-        ]
-        : [ { maxInstances: 1, browserName: "chrome" } ],
+    capabilities: [
+        {
+            browserName: "chrome",
+            "goog:chromeOptions": {
+                args: debug
+                    ? []
+                    : [
+                          "--no-sandbox",
+                          "--headless",
+                          "--disable-gpu",
+                          "--disable-extensions",
+                      ],
+            },
+        },
+    ],
     sync: true,
     logLevel: "silent",
     coloredLogs: true,
     bail: 0,
     screenshotPath: "dist/wdio/",
-    baseUrl: debug ? "http://localhost:8080/" : "https://charts102-sandbox.mxapps.io/",
+    baseUrl: debug
+        ? "http://localhost:8080/"
+        : "https://charts102-sandbox.mxapps.io/",
     waitforTimeout: 180000,
     connectionRetryTimeout: 200000,
     connectionRetryCount: 2,
     killInstances: true,
-    services:  multiclient ? [ "iedriver", "selenium-standalone" ] : [ "selenium-standalone" ],
-    seleniumArgs: multiclient ? {
-        javaArgs: [
-            "-Dwebdriver.edge.driver=C:\\Selenium\\EdgeDriver\\MicrosoftWebDriver.exe"
-        ]
-    }
-    : {},
+    services: ["selenium-standalone"],
     framework: "jasmine",
-    reporters: [ "dot", "spec" ],
-    execArgv: debug ? [ "--inspect" ] : undefined,
+    reporters: ["dot", "spec"],
+    execArgv: debug ? ["--inspect"] : undefined,
     jasmineNodeOpts: {
-        defaultTimeoutInterval: debug ? (60 * 60 * 1000) : (100 * 1000),
-        expectationResultHandler: function(passed, assertion) {
+        defaultTimeoutInterval: debug ? 60 * 60 * 1000 : 100 * 1000,
+        expectationResultHandler: function (passed, assertion) {
             if (passed) {
                 return;
             }
             browser.saveScreenshot(
                 "dist/wdio/assertionError_" + assertion.error.message + ".png"
             );
-        }
-    }
+        },
+    },
 };
