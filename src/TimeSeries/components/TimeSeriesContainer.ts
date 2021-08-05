ad16f6dcb968d131353903cf013b7d7668869792
Diego Antonelli <diego.antonelli@mendix.com>
2021-08-05 10:05:23 +0200

    Missing baseUrl change

--- src/TimeSeries/components/TimeSeriesContainer.ts
+++ src/TimeSeries/components/TimeSeriesContainer.ts
@@ -2,7 +2,7 @@ let __webpack_public_path__: string;
 import { SFC, createElement } from "react";
 import LineChartContainer from "../../LineChart/components/LineChartContainer";
 import { Container } from "../../utils/namespaces";
-__webpack_public_path__ = window.mx.baseUrl + "../widgets/";
+__webpack_public_path__ = window.mx.remoteUrl + "widgets/";
 
 const TimeSeriesContainer: SFC<Container.LineChartContainerProps> = props =>
     createElement(LineChartContainer, {
